3c8208895a1c2e725974f30833d5e8118e090cd7
Maestro1982 <kris.tote@telenet.be>
2022-10-26 16:02:20 +0200

    create product component, tailwind css styling, create api, util helpers

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,21 @@ import Header from '../components/Header';
 import Landing from '../components/Landing';
 import { Tab } from '@headlessui/react';
 import { fetchCategories } from '../utils/fetchCategories';
+import { fetchProducts } from '../utils/fetchProducts';
+import Product from '../components/Product';
 
 interface Props {
   categories: Category[];
+  products: Product[];
 }
 
-const Home = ({ categories }: Props) => {
+const Home = ({ categories, products }: Props) => {
+  console.log(products);
+  const showProducts = (category: number) => {
+    return products
+      .filter((product) => product.category._ref === categories[category]._id)
+      .map((product) => <Product product={product} key={product._id} />); // Filter products by category
+  };
   return (
     <div className=''>
       <Head>
@@ -44,10 +53,10 @@ const Home = ({ categories }: Props) => {
               ))}
             </Tab.List>
             <Tab.Panels className='mx-auto max-w-fit pt-10 pb-24 sm:px-4'>
-              {/* <Tab.Panel className='tabPanel'>{showProducts(0)}</Tab.Panel>
+              <Tab.Panel className='tabPanel'>{showProducts(0)}</Tab.Panel>
               <Tab.Panel className='tabPanel'>{showProducts(1)}</Tab.Panel>
               <Tab.Panel className='tabPanel'>{showProducts(2)}</Tab.Panel>
-              <Tab.Panel className='tabPanel'>{showProducts(3)}</Tab.Panel>  */}
+              <Tab.Panel className='tabPanel'>{showProducts(3)}</Tab.Panel>
             </Tab.Panels>
           </Tab.Group>
         </div>
@@ -62,9 +71,11 @@ export default Home;
    Pages are already loaded before the client asks, so the loading time is removed. */
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
   const categories = await fetchCategories();
+  const products = await fetchProducts();
   return {
     props: {
       categories,
+      products,
     },
   };
 };
